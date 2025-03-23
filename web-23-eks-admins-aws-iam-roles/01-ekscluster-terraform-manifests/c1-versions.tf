19e9e4004a9fa1a8fcd1adf734049d4635b2e479
bimalkeeth <bimalkeeth@gmail.com>
2025-03-24 05:39:19 +1100

    commented private node group

---
+++ web-23-eks-admins-aws-iam-roles/01-ekscluster-terraform-manifests/c1-versions.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_version = ">= 0.14"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.35.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "terraform-on-aws-eks-bimal"
+    key    = "dev/eks-cluster/terraform.tfstate"
+    region = "us-east-1"
+    #used for state locking
+
+    dynamodb_table = "dev-ekscluster"
+  }
+
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
